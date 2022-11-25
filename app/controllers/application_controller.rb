4791ad23903dd75e1f20dc58fa2322ede92b8818
Rattlehead90 <strogonov@seznam.cz>
2022-11-25 10:08:11 +0100

    Add name field to devise edit account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
 
   protected
 
@@ -7,4 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
   end
 
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+  end
 end
