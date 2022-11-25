82eaad47f251f39e518e175664ef5c51d7e5d8d9
Rattlehead90 <strogonov@seznam.cz>
2022-11-25 10:01:41 +0100

    Add name field to devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, only: [:create]
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+  end
+
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = current_user.build_event(event_params)
+
+    if @event.save 
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date)
+  end
 end
