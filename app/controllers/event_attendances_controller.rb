37dc09051ff8d54e061c14af74394b98a36cfc6f
Rattlehead90 <strogonov@seznam.cz>
2022-11-27 11:51:49 +0100

    Add event_attendance#create

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,5 +1,15 @@
 class EventAttendancesController < ApplicationController
   def create
+    event = Event.find(params[:id])
+    attendance = Event_attendance.new
+    attendance.attended_event = event
+    attendance.attendee = current_user
+
+    if attendance.save
+      redirect_to event
+    else
+      render :show, status: :unprocessable_entity
+    end
   end
 
   def destroy
