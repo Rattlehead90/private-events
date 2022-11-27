c131c6c5945928b36e4022c2a7e2ffcbbf2361e5
Rattlehead90 <strogonov@seznam.cz>
2022-11-27 11:21:40 +0100

    Add controller for attendances

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'event_attendances/create'
+  get 'event_attendances/destroy'
+  get 'event_attendances/update'
   devise_for :users
   root 'events#index'
 
-  resources :events  
+  resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
