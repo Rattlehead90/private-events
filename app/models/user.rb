8a6b7d9ad864830fa9117f80ceb73d018747dbbf
Rattlehead90 <strogonov@seznam.cz>
2022-11-28 20:32:33 +0100

    Add dependend destroy to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   has_many :event_attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendances
+  has_many :attended_events, through: :event_attendances, dependent: :destroy
 end
